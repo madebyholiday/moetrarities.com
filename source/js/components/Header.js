f887959eb4d6890ba1caebbe1832b0b0806578bd
richardcalahan <richard@calahan.me>
2021-05-25 11:58:22 -0700

    header 80%

--- source/js/components/Header.js
+++ source/js/components/Header.js
@@ -8,20 +8,26 @@ class Header {
   constructor () {
     this.$el = $('.js-header')
     this.$window = $(window)
+    this.$document = $(document)
     this.$body = $('body')
     this.$mobileMenu = this.$el.find('.js-mobile-menu');
     this.$menuToggle = this.$el.find('.js-menu-toggle')
 
     this.onMobileMenuItemHasLinkListsClick = this.onMobileMenuItemHasLinkListsClick.bind(this)
     this.onMenuToggleClick = this.onMenuToggleClick.bind(this)
-    this.onWindowResize = this.onWindowResize.bind(this)
+    this.onNavPrimaryItemClick = this.onNavPrimaryItemClick.bind(this)
+    this.onDocumentClick = this.onDocumentClick.bind(this)
+    this.onWindowResize = this.onWindowResize.bind(this)    
 
     this.$menuToggle.on('click', this.onMenuToggleClick)
-    this.$window.on('resize', this.onWindowResize)
+    this.$document.on('click', this.onDocumentClick)
+    this.$window.on('resize', this.onWindowResize);
     
     this.$mobileMenu
         .find('.js-has-link-lists > a')
         .on('click', this.onMobileMenuItemHasLinkListsClick)
+    
+    this.$el.find('.js-nav-primary-item > a').on('click', this.onNavPrimaryItemClick)
     
   }
 
@@ -57,6 +63,49 @@ class Header {
     })    
   }
 
+  openDropdown ( id ) {
+    if ( this.activeDropdownId )
+      this.closeDropdown(this.activeDropdownId)
+
+    if ( this.activeDropdownId === id )
+      return
+    
+    this.activeDropdownId = id
+    
+    let $dropdown = $(`.js-nav-dropdown[data-id="${id}"]`)
+
+    if ( this.dropdownTl )
+      this.dropdownTl.kill()
+    
+    this.dropdownTl = gsap.timeline()
+    
+    this.dropdownTl.fromTo($dropdown, .3, {
+      y: 0,
+      autoAlpha: 1
+    }, {
+      y: '100%',
+      ease: 'power4.out'
+    })
+  }
+  
+  closeDropdown ( id ) {
+    let $dropdown = $(`.js-nav-dropdown[data-id="${id}"]`)
+    
+    if ( this.dropdownTl )
+      this.dropdownTl.kill()
+    
+    this.dropdownTl = gsap.timeline({
+      onComplete: () => {
+        this.activeDropdownId = null
+      }
+    })
+    
+    this.dropdownTl.to($dropdown, 0, {
+      autoAlpha: 0,
+      ease: 'power4.inOut'
+    })
+  }
+
   onMenuToggleClick () {
     if ( this.$body.hasClass('is-menu-open') ) {
       this.closeMobileMenu()
@@ -81,9 +130,27 @@ class Header {
     }
   }
 
+  onNavPrimaryItemClick ( e ) {
+    e.preventDefault()
+
+    let $this = $(e.currentTarget),
+        $li = $this.closest('li'),
+        id = $li.data('id')
+
+    this.openDropdown(id)
+  }
+
   onWindowResize () {
     if ( isDesktop() && this.$body.hasClass('is-menu-open') )
       this.closeMobileMenu()
+  }
+
+  onDocumentClick ( e ) {
+    let $this = $(e.target)
+
+    if ( !$this.closest('.js-header').length &&
+         this.activeDropdownId )
+      this.closeDropdown(this.activeDropdownId)    
   }
 }
 
