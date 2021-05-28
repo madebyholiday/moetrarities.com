36b06f3f3f139ddac9dc87caaace2348235a3375
richardcalahan <richard@calahan.me>
2021-05-28 15:28:01 -0700

    sdf

--- source/js/views/Product.js
+++ source/js/views/Product.js
@@ -7,12 +7,37 @@ class Product {
     this.accordion = new Accordion($('.js-accordion'))
     this.tabs = new Tabs($('.js-tabs'))
     this.mediaGallery = new MediaGallery($('.js-media-gallery'))
+
+    this.$productMedia = $('.js-product-media')
+
+    this.onProductMediaClick = this.onProductMediaClick.bind(this)
+    this.onProductMediaBeforeChange = this.onProductMediaBeforeChange.bind(this)
+
+    this.$productMedia.slick({
+      rows: 0,
+      adaptiveHeight: true,
+      arrows: false,
+      dots: true
+    })
+
+    this.$productMedia.on('beforeChange', this.onProductMediaBeforeChange)
+    this.$productMedia.on('click', this.onProductMediaClick)
   }
 
   afterLeave ( data ) {
     this.accordion.destroy()
     this.tabs.destroy()
-    this.mediaGallery.destroy()
+    this.mediaGallery.destroy()    
+  }
+
+  onProductMediaClick () {
+    this.$productMedia.slick('slickNext')
+  }
+
+  onProductMediaBeforeChange () {
+    this.$productMedia.find('img').each((k,v) => {
+      lazySizes.loader.unveil(v)
+    })
   }
   
   get namespace () {
