aa44a8e8c81034535e001ac747e68e3e64851ef1
richardcalahan <richard@calahan.me>
2021-07-30 13:54:06 -0700

    sdf

--- source/js/components/PageLoader.js
+++ source/js/components/PageLoader.js
@@ -1,31 +1,85 @@
 import gsap from 'gsap'
+import Preload from 'preload-it'
+import Promise from 'bluebird'
+import map from 'lodash/map'
+import flattenDeep from 'lodash/flattenDeep'
+
+import {
+  isTablet
+} from 'utils/media'
+
+let preloader = Preload()
 
 class PageLoader {
   constructor () {
     this.$el = $('.js-page-loader')
     this.$window = $(window)
     this.$body = $('body')
-    
-    this.tl = gsap.timeline({
-      onUpdate: () => {
-        if ( this.tl.progress() >= .75 && !this.triggeredWillReveal ) {
-          this.$window.trigger('willreveal')
-          this.triggeredWillReveal = true
-        }
-      },
-      onComplete: () => {
-        this.$el.remove()
-      }
-    })
+    this.$current = $('.js-current')
 
-    if ( process.env.NODE_ENV === 'production' ) {
+    this.urls = []    
+
+    this.onPreloadProgress = this.onPreloadProgress.bind(this)
+    this.onPreloadComplete = this.onPreloadComplete.bind(this)        
+
+    preloader.onprogress = this.onPreloadProgress
+
+    this.preload().then(() => {
+      this.tl = gsap.timeline({
+        onUpdate: () => {
+          if ( this.tl.progress() >= .2 && !this.triggeredWillReveal ) {
+            this.$window.trigger('willreveal')
+            this.triggeredWillReveal = true
+          }
+        },
+        onComplete: () => {
+          this.$el.remove()
+        }
+      })
+      this.tl.to(this.$el.find('.js-content'), 2, {
+        autoAlpha: 0,
+        delay: .5,
+        ease: 'power4.out'
+      })
+
+      this.tl.to(this.$el, 2, {
+        autoAlpha: 0,
+        ease: 'power4.out'
+      }, '-=.5')
+    })    
+  }
+
+  preload () {
+    return new Promise(resolve => {
+      if ( isTablet() ) {
+        this.urls = flattenDeep(
+          map($('[data-preload]'), el => $(el).data('preload'))
+        )
+      } else {
+        this.urls = flattenDeep(
+          map($('[data-preload-mobile]'), el => $(el).data('preload-mobile'))
+        )
+      }      
       
-    }
+      if ( this.urls.length ) {
+        preloader
+          .fetch(this.urls)
+          .then(this.onPreloadComplete)
+          .then(resolve)
+      } else {
+        resolve()
+      }           
+    })                    
+  }
+
+  onPreloadProgress ( e ) {
+    this.$current.width(`${e.progress}%`)
+  }
+
+  onPreloadComplete ( items ) {
+    this.$current.width(`100%`)
     
-    this.tl.to(this.$el, 3, {
-      autoAlpha: 0,
-      ease: 'power4.out'
-    })
+    return Promise.resolve(items)
   }
 
   destroy () {
--- source/js/views/Home.js
+++ source/js/views/Home.js
@@ -3,6 +3,10 @@ import 'debug.addIndicators'
 import { cover, contain } from 'intrinsic-scale'
 import gsap from 'gsap'
 
+import {
+  isTablet
+} from 'utils/media'
+
 class Home {
   beforeEnter ( data ) {
     this.$window = $(window)
@@ -11,87 +15,122 @@ class Home {
     
     this.initScene = this.initScene.bind(this)
     this.onWindowResize = this.onWindowResize.bind(this)
+    this.onWindowResizeend = this.onWindowResizeend.bind(this)
+    this.onWindowWillreveal = this.onWindowWillreveal.bind(this)
     
-    this.controller = new ScrollMagic.Controller
-
-    this.setCanvasDims()
+    this.controller = new ScrollMagic.Controller({
+      addIndicators: true
+    })
 
     this.$window.on('resize', this.onWindowResize)
-    this.$scenes.each(this.initScene)
+    this.$window.on('resizeend', this.onWindowResizeend)
+    this.$window.on('willreveal', this.onWindowWillreveal)
   }
 
-  initScene (index, element) {
-    let $this = $(element),
-        images = $this.data('images').sort(),
-        $canvas = $this.find('.js-canvas'),
-        $text = $this.find('.js-text')
-
-    let imgs = []
+  initScenes () {
+    this.$scenes.each(this.initScene)       
+    this.setCanvasDims()
+  }  
+  
+  initScene ( index, element ) {
+    let $element = $(element),
+        type = $element.data('type')
     
+    switch ( type ) {
+      case 'media':
+        return this.initMedia(element)
+      case 'productIntro':
+        return this.initProductIntro(element)
+      case 'productDetail':
+        return this.initProductDetail(element)
+      case 'quote':
+        return this.initQuote(element, index)
+      case 'text':
+        return this.initText(element, index)
+    }
+  }
+
+  initProductIntro ( element ) {
+    let $this = $(element),
+        images = isTablet() ?
+                 $this.data('images').sort() :
+                 $this.data('images-mobile').sort(),
+        $text = $this.find('.js-text'),
+        $canvas = $this.find('.js-canvas')
+
+    let pxPerImg = 50,
+        imgs = [],
+        tl
+
     $.each(images, (k,v) => {
       let img = new Image()
       img.src = v
       imgs.push(img)
     })
 
-    let pxPerImg = 25,
-        pxEndPause = $this.data('pause-duration')   
+    tl = gsap.timeline({ paused: true })
+    
+    tl.fromTo($canvas, .5, {
+      autoAlpha: 0
+    }, {
+      autoAlpha: 1
+    }, 'in')
+
+    tl.fromTo($text, 1, {
+      autoAlpha: 0,
+      y: 30
+    }, {
+      autoAlpha: 1,
+      y: 0,
+      ease: 'power4.in'
+    }, 'in')
+
+    tl.fromTo($text, 3, {
+      autoAlpha: 1
+    }, {
+      autoAlpha: 1
+    }, 'texton')
+
+    tl.fromTo($text, 1, {
+      autoAlpha: 1
+    }, {
+      autoAlpha: 0,
+      y: -30,
+      ease: 'power4.in'
+    })
+    
+    tl.fromTo($canvas, 1, {
+      autoAlpha: 1
+    }, {
+      autoAlpha: 0
+    }, 'out')
+
+    tl.progress(1)
     
     let scene = new ScrollMagic.Scene({
       triggerHook: 0,
       triggerElement: element,
-      duration: (imgs.length * pxPerImg) + pxEndPause
-    }).on('enter', e => {
-      
-    }).on('progress', e => {      
-      let canvasO
-
-      if ( e.progress <= .15 ) {
-        canvasO = (.15 + (e.progress - .15)) / .15
-      }
-
-      if ( e.progress > .9 ) {
-        canvasO = (1 - e.progress) / .1
-      }
-
-      gsap.set($canvas, {
-        autoAlpha: canvasO
-      })
-      
-      if ( $text.length ) {
-        let textO,
-            textY
-
-        if ( e.progress <= .5 ) {
-          textO = (.5 + (e.progress - .5)) / .5
-          textY = 50 - (textO * 50)
-        }        
-        
-        if ( e.progress > .9 ) {
-          textO = (1 - e.progress) / .1
-          textY = (textO * 50) - 50
-        }
-
-        gsap.set($text, {
-          autoAlpha: textO,
-          y: textY
-        })
-      }
-
-      // percentage to shorten the sequence by, so we can pause
-      // for pxEndPause number of px at the end of the sequence
-      let perc = 1 - Math.floor(pxEndPause / pxPerImg) / (imgs.length - 1)
-      
-      let index = Math.floor((e.progress / perc) * (imgs.length - 1)),
+      duration: (imgs.length * pxPerImg)
+    }).on('enter', e => {      
+      this.setCanvasDims()      
+    }).on('progress', e => {
+      let index = Math.floor(e.progress * imgs.length),
           img = imgs[index]
-      
+
+      tl.progress(Math.round(e.progress * 100) / 100)
+
       if ( img )
-        requestAnimationFrame(() => {        
-          let { width, height, x, y } = cover(
+        requestAnimationFrame(() => {
+          let { width, height, x, y } = isTablet() ? cover(
             $canvas.width(),
             $canvas.height(),
             4,
             3
+          ) : cover(
+            $canvas.width(),
+            $canvas.height(),
+            5,
+            7
           )
 
           let context = $canvas[0].getContext('2d')
@@ -105,13 +144,259 @@ class Home {
             height * devicePixelRatio
           )       
         })      
-    }).on('leave', e => {
+    }).setPin(element).addTo(this.controller)    
+  }
+
+  initProductDetail ( element ) {
+    let $this = $(element),
+        images = isTablet() ?
+                 $this.data('images').sort() :
+                 $this.data('images-mobile').sort(),
+        $text = $this.find('.js-text'),
+        $canvas = $this.find('.js-canvas')
+
+    let pxPerImg = 50,
+        imgs = [],
+        tl
+
+    $.each(images, (k,v) => {
+      let img = new Image()
+      img.src = v
+      imgs.push(img)
+    })
+
+    tl = gsap.timeline({ paused: true })
+    
+    tl.fromTo($canvas, .5, {
+      autoAlpha: 0
+    }, {
+      autoAlpha: 1
+    }, 'in')
+
+    tl.fromTo($text, 2, {
+      autoAlpha: 0,
+      y: 30
+    }, {
+      autoAlpha: 1,
+      delay: 4,
+      y: 0
+    })
+
+    tl.fromTo($text, 3, {
+      autoAlpha: 1
+    }, {
+      autoAlpha: 1
+    }, 'texton')
+
+    tl.fromTo($text, 1, {
+      autoAlpha: 1
+    }, {
+      autoAlpha: 0,
+      y: -30
+    }, 'out')
+    
+    tl.fromTo($canvas, 1, {
+      autoAlpha: 1
+    }, {
+      autoAlpha: 0
+    }, 'out')
+
+    tl.progress(1)
+    
+    let scene = new ScrollMagic.Scene({
+      triggerHook: 0,
+      triggerElement: element,
+      duration: (imgs.length * pxPerImg)
+    }).on('enter', e => {      
+      this.setCanvasDims()      
+    }).on('progress', e => {
+      let index = Math.floor(e.progress * imgs.length),
+          img = imgs[index]
+
+      tl.progress(Math.round(e.progress * 100) / 100)
+
+      if ( img )
+        requestAnimationFrame(() => {
+          let { width, height, x, y } = isTablet() ? cover(
+            $canvas.width(),
+            $canvas.height(),
+            4,
+            3
+          ) : cover(
+            $canvas.width(),
+            $canvas.height(),
+            5,
+            7
+          )
+
+          let context = $canvas[0].getContext('2d')
+          context.drawImage(
+            img,
+            0, 0,
+            img.width, img.height,
+            x * devicePixelRatio,
+            y * devicePixelRatio,
+            width * devicePixelRatio,
+            height * devicePixelRatio
+          )       
+        })      
+    }).setPin(element).addTo(this.controller)    
+  }
+  
+  initMedia ( element ) {
+    let $this = $(element),
+        images = isTablet() ?
+                 $this.data('images').sort() :
+                 $this.data('images-mobile').sort(),
+        $canvas = $this.find('.js-canvas')
+
+    let pxPerImg = 25,
+        imgs = []
+
+    $.each(images, (k,v) => {
+      let img = new Image()
+      img.src = v
+      imgs.push(img)
+    })
+
+    let tl = gsap.timeline({ paused: true })
+    
+    tl.fromTo($canvas, 1.5, {
+      autoAlpha: 0,
+      y: 20
+    }, {
+      autoAlpha: 1,
+      y: 0
+    }, 'in')      
+
+    let scene = new ScrollMagic.Scene({
+      triggerHook: 0,
+      triggerElement: element,
+      duration: (imgs.length * pxPerImg)
+    }).on('enter', e => {            
+      this.setCanvasDims()
+    }).on('progress', e => {
+      let index = Math.floor(e.progress * imgs.length),
+          img = imgs[index]
+
+      tl.progress(Math.round(e.progress * 100) / 100)
       
-    }).setPin(element).addTo(this.controller)
+      if ( img )
+        requestAnimationFrame(() => {
+          let { width, height, x, y } = isTablet() ? cover(
+            $canvas.width(),
+            $canvas.height(),
+            4,
+            3
+          ) : cover(
+            $canvas.width(),
+            $canvas.height(),
+            5,
+            7
+          )
+
+          let context = $canvas[0].getContext('2d')
+          context.drawImage(
+            img,
+            0, 0,
+            img.width, img.height,
+            x * devicePixelRatio,
+            y * devicePixelRatio,
+            width * devicePixelRatio,
+            height * devicePixelRatio
+          )       
+        })      
+    }).setPin(element).addTo(this.controller)    
+  }
+
+  initQuote ( element, index ) {
+    let $this = $(element),
+        $text = $this.find('.js-text')
+
+    let tl = gsap.timeline({ paused: true })
+    tl.fromTo($text, 1.5, {
+      autoAlpha: 0,
+      y: 20
+    }, {
+      autoAlpha: 1,
+      y: 0
+    }, 'in')
+
+    if ( index !== this.$scenes.length - 1 ) {
+      tl.fromTo($text, 3, {
+        autoAlpha: 1
+      }, {
+        autoAlpha: 1
+      }, 'on')
+      tl.fromTo($text, 1.5, {
+        autoAlpha: 1,
+        y: 0
+      }, {
+        autoAlpha: 0,
+        y: -20
+      }, 'out')
+      
+      tl.progress(1)
+    }
+    
+    let scene = new ScrollMagic.Scene({
+      triggerHook: 0,
+      triggerElement: element,
+      duration: 3000
+    }).on('enter', e => {
+      
+    }).on('progress', e => {
+      tl.progress(Math.round(e.progress * 100) / 100)                 
+    }).setPin(element).addTo(this.controller)        
+  }
+
+  initText ( element, index ) {
+    let $this = $(element),
+        $text = $this.find('.js-text')
+
+    let tl = gsap.timeline({ paused: true })
+
+    if ( index !== 0 )
+      tl.fromTo($text, 1.5, {
+        autoAlpha: 0,
+        y: 20
+      }, {
+        autoAlpha: 1,
+        y: 0
+      }, 'in')
+
+    if ( index !== this.$scenes.length - 1 ) {
+      tl.fromTo($text, 2, {
+        autoAlpha: 1
+      }, {
+        autoAlpha: 1
+      }, 'on')
+      tl.fromTo($text, 1.5, {
+        autoAlpha: 1,
+        y: 0
+      }, {
+        autoAlpha: 0,
+        y: -20
+      }, 'out')
+      
+      tl.progress(1)
+    }
+    
+    let scene = new ScrollMagic.Scene({
+      triggerHook: 0,
+      triggerElement: element,
+      duration: window.innerHeight
+    }).on('enter', e => {
+      
+    }).on('progress', e => {
+      tl.progress(Math.round(e.progress * 100) / 100)                 
+    }).setPin(element).addTo(this.controller)        
   }
 
   afterLeave ( data ) {
     this.$window.off('resize', this.onWindowResize)
+    this.$window.off('willreveal', this.onWindowWillreveal)
+    this.controller.destroy()
   }
 
   setCanvasDims () {
@@ -123,9 +408,18 @@ class Home {
       $canvas.height(window.innerHeight)
     })
   }
+
+  onWindowWillreveal () {
+    this.initScenes()
+  }
   
   onWindowResize () {
     this.setCanvasDims()
+  }
+  
+  onWindowResizeend () {
+    if ( isTablet() )
+      window.location.reload()
   }
   
   get namespace () {
